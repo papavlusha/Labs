1ef8d97927b1cd1184c43289bf1c269ffb152598
papavlusha <95187375+papavlusha@users.noreply.github.com>
2022-03-01 10:13:26 +0300

    Add files via upload
    Rogovtsov, laba 1

---
+++ Assembler lab 1 v 2.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int Asm_part(int a, int b) {
+    int answer;
+    __asm {
+        mov eax, b              //eax <=b
+        neg eax                 // eax <= -b
+        mov ebx, a              //ebx <= a
+        cdq                     // расширение для деления
+        idiv ebx                // eax << -b/a
+        mov answer, eax         //answer <= -b/a
+    }
+    return answer;
+}
+
+int main() {
+    int a, b;
+    cout << "solution ax+b=0\n";
+    cout << "enter a: ";
+    cin >> a;
+    cout << "enter b: ";
+    cin >> b;
+    cout << Asm_part(a,b);
+}
